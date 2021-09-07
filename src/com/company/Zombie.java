ce6782303d55a24ba8356c060352584c8bae4ce6
DAderal <mr.andreibibik@gmail.com>
2021-09-07 18:04:35 +0700

    Initial commit

---
+++ src/com/company/Zombie.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Random;
+
+public class Zombie extends Character{
+    public Zombie(String name, int healthPoints, int strength, int dexterity, int xp, int gold){
+        super(name, healthPoints, strength, dexterity, xp, gold);
+    }
+
+}
